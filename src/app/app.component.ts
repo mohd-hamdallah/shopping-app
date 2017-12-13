77528f4290af3ae4d147abff482b623ea1a37266
naifsaleh <naifalsaleh3000@gmail.com>
2017-12-13 15:44:48 +0300

    add rtl feature

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,3 +1,4 @@
+import { TranslateService, LangChangeEvent } from '@ngx-translate/core';
 import { Component } from '@angular/core';
 
 @Component({
@@ -6,4 +7,11 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
+  dir: string;
+
+  constructor(translateService: TranslateService) {
+    translateService.onLangChange.subscribe((langChangeEvent: LangChangeEvent) => {
+      this.dir = (langChangeEvent.lang === 'ar') ? 'rtl' : 'ltr';
+    });
+  }
 }
